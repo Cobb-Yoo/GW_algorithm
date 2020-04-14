8ba5172486ae157955e9ea604f887c28aeb12188
YooCobb <yhj010597@naver.com>
2020-04-15 08:22:46 +0900

    2

--- 크러스컬2.cpp
+++ 크러스컬2.cpp
@@ -2,7 +2,11 @@
 #include <algorithm>
 using namespace std;
 
+void init_set(int *arr, int n){
+	for(int i=0;i<n;i++) arr[i] = i;
+}
 bool cmp(vector<int> a, vector<int> b){
+	if(a[2] == b[2]) return a[0] < b[0];
 	return a[2] < b[2];
 }
 
@@ -12,9 +16,9 @@ int find(int n, int *arr){
 }
 
 int main(){
-	int answer = 0;
+	int answer = 0;// 최소로 연결되는 비용의 합 
 	vector<vector<int>> v; // x, y, cost
-	
+	int n = 9;
 	v.push_back({0,1,4}); // 0과 1은 정점이고 4는 비용 
 	v.push_back({0,7,8});
 	v.push_back({1,2,8});
@@ -30,22 +34,28 @@ int main(){
 	v.push_back({6,8,6});
 	v.push_back({7,8,7});
 	
-	int len = v.size();
-	int arr[len];
-	
-	for(int i=0;i<len;i++) arr[i] = i; //init_set();
-	
 	sort(v.begin(), v.end(), cmp); // 비용에 따른 오름차순으로 퀵sort 
 	
+	int len = v.size();
+	int parent[n];// 가장 최상위에 있는 부모노드를 알려주는 배열
+	
+	init_set(parent, n);
+	
 	for(int i=0;i<len;i++){
-		int x = find(v[i][0], arr);
-		int y = find(v[i][1], arr);
-		
-		if(x != y) { // union();
-			arr[x] = y;
-			answer += v[i][2]; // cost
+		int startPoint_parent = find(v[i][0], parent);
+		int destination_parent = find(v[i][1], parent);
+		int cost = v[i][2];
+		if(startPoint_parent != destination_parent) { 
+			parent[startPoint_parent] = destination_parent; // union();
+			answer += cost; // cost
 		}
+		
+		for(int j=0;j<n;j++){
+			cout << parent[j] << " ";
+		}
+		cout << endl;
 	}
 	
 	cout << answer << endl;
+	
 }
