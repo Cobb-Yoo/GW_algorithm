143b5b3f45e105a9cfd773928314a7247ac1cd48
YooCobb <yhj010597@naver.com>
2020-04-23 16:08:58 +0900

    범위인접행렬로 입력값을 바꿈

--- 크러스컬4.cpp
+++ 크러스컬4.cpp
@@ -40,20 +40,26 @@ int main(){
 	int answer = 0;// 최소로 연결되는 비용의 합 
 	vector<vector<int>> v; // x, y, cost
 	int n = 9;
-	v.push_back({0,1,4}); // 0과 1은 정점이고 4는 비용 
-	v.push_back({0,7,8});
-	v.push_back({1,2,8});
-	v.push_back({1,7,11});
-	v.push_back({2,3,7});
-	v.push_back({2,5,4});
-	v.push_back({2,8,2});
-	v.push_back({3,4,9});
-	v.push_back({3,5,14});
-	v.push_back({4,5,10});
-	v.push_back({5,6,2});
-	v.push_back({6,7,1});
-	v.push_back({6,8,6});
-	v.push_back({7,8,7});
+
+	int datas[n][n] = {{0,4,0,0,0,0,0,8,0},
+					{4,0,8,0,0,0,0,11,0},
+					{8,0,0,7,0,4,0,0,2},
+					{0,0,7,0,9,14,0,0,0},
+					{0,0,0,9,0,10,0,0,0},
+					{0,0,4,14,10,0,2,0,0},
+					{0,0,0,0,0,2,0,1,0},
+					{8,11,0,0,0,0,1,0,7},
+					{0,0,2,0,0,0,0,7,0}};
+					
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(i == j || datas[i][j] == 0) continue;
+			
+			v.push_back({i,j,datas[i][j]});
+			datas[i][j] = 0;
+			datas[j][i] = 0;
+		}
+	}
 	
 	sort(v.begin(), v.end(), cmp); // 비용에 따른 오름차순으로 퀵sort 
 	
