a65c135dd175027de249097b8e5b3f6e2cd6da2c
YooCobb <yhj970105@gmail.com>
2020-05-26 00:14:28 +0900

    merge 배열을 미리 정해서 일어나는 필요없는 메모리 값을 줄이기 위해서 vector로 필요한 만큼만 선언하게 끔 변경

--- quick and merge/merge/merge1.cpp
+++ quick and merge/merge/merge1.cpp
@@ -4,28 +4,23 @@
 #include <ctime>
 using namespace std;
 
-const int MAX = 100000;
-int cnt=0;
-
 void merge(vector<int> &v, int low, int middle, int high){
-	int n1=low, n2=middle+1, s = low;
-	int sorted[MAX];
+	int n1=low, n2=middle+1, cnt = 0;
+	vector<int> sub_v;
 	
 	while(n1 <= middle && n2 <= high){
-		if(v[n1] < v[n2]) sorted[s++] = v[n1++];
-		else sorted[s++] = v[n2++];
+		if(v[n1] < v[n2]) sub_v.push_back(v[n1++]);
+		else if(v[n1] > v[n2]) sub_v.push_back(v[n2++]);
 	}
-	if(n1 > middle) while(n2 <= high) sorted[s++] = v[n2++];
-	else while(n1 <= middle) sorted[s++] = v[n1++];
 	
-	for(int i=low;i<=high;i++) v[i] = sorted[i];
+	while(n1 <= middle) sub_v.push_back(v[n1++]);
+	while(n2 <= high) sub_v.push_back(v[n2++]);
+	
+	for(int i=low;i<=high;i++) v[i] = sub_v[cnt++];
 }
 
 void mergesort(vector<int> &v, int low, int high){	
-	cnt++;
 	if(low < high){
-		// 혹시나 (high+low) 에서 오버플로우가 발생할 수 있기 때문에
-		//같은 연산이나 마찬가지인 연산을 채택 
 		int middle = low + (high - low) / 2;
 		mergesort(v, low, middle);
 		mergesort(v, middle+1, high);
@@ -37,13 +32,11 @@ int main(){
 	int n;
 	cin >> n;
 	vector<int> v(n);
-	int tmp=0;
 	srand((unsigned int)time(NULL));
 	
 	for(int i=0;i<n;i++) v[i] = i+1;
 	
 	for(int i=0;i<100000;i++){
-		tmp++;
 		int x = rand() % n;
 		int y = rand() % n;
 		
@@ -57,10 +50,6 @@ int main(){
 	float s = clock();
 	mergesort(v,0,n-1);
 	float f = clock();
-	
-	//for(int i=0;i<n;i++) cout << v[i] << " ";
-	cout << endl;	
-	cout << cnt << " " << tmp << endl;
 	
 	cout << (float)(f-s)/CLOCKS_PER_SEC << "초 걸렸습니다. "<< endl;
 }
